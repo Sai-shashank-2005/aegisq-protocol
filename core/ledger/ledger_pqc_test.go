4e3366b02dc833a0c2aff7d7c9c33fdd8ec9f3b3
Sai-shashank-2005 <saishashankpedapati@gmail.com>
2026-02-28 09:56:21 +0530

    Layer 6 consensus implemented

--- core/ledger/ledger_pqc_test.go
+++ core/ledger/ledger_pqc_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/Sai-shashank-2005/aegisq-protocol/core/block"
+	"github.com/Sai-shashank-2005/aegisq-protocol/core/consensus"
 	"github.com/Sai-shashank-2005/aegisq-protocol/core/crypto"
 	"github.com/Sai-shashank-2005/aegisq-protocol/core/identity"
 	"github.com/Sai-shashank-2005/aegisq-protocol/core/transaction"
@@ -13,37 +14,69 @@ func TestFullChainWithDilithium(t *testing.T) {
 
 	signer, err := crypto.NewDilithiumSigner()
 	if err != nil {
-		t.Fatal("Failed to initialize Dilithium signer")
+		t.Fatal(err)
+	}
+	defer signer.Close()
+
+	// Create validator identity
+	node, err := identity.NewNodeIdentity("validator-1", signer)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	node, err := identity.NewNodeIdentity("validator-pqc", signer)
-	if err != nil {
-		t.Fatal("Failed to create identity")
+	// Create validator set
+	vs := consensus.NewValidatorSet()
+	vs.AddValidator("validator-1", node.PublicKey)
+
+	// --- Genesis block must contain a transaction ---
+	genesisTx := transaction.NewTransaction(
+		node,
+		"genesis_payload",
+		"genesis_data",
+	)
+
+	if err := genesisTx.SignWithIdentity(node); err != nil {
+		t.Fatal(err)
 	}
 
-	// Genesis block
-	txGenesis := transaction.NewTransaction(node, "genesis", "pqc")
-	txGenesis.SignWithIdentity(node)
+	genesis := block.NewBlock(
+		0,
+		[]byte("genesis"),
+		[]*transaction.Transaction{genesisTx},
+	)
 
-	genesis := block.NewBlock(0, []byte("genesis"), []*transaction.Transaction{txGenesis})
-	genesis.Finalize(node)
-
-	ledger := NewLedger(genesis)
-
-	// Block 1
-	tx1 := transaction.NewTransaction(node, "data1", "meta1")
-	tx1.SignWithIdentity(node)
-
-	block1 := block.NewBlock(1, genesis.Hash, []*transaction.Transaction{tx1})
-	block1.Finalize(node)
-
-	err = ledger.AddBlock(block1, signer, node.PublicKey)
-	if err != nil {
-		t.Fatal("Failed to add PQC block")
+	if err := genesis.Finalize(node); err != nil {
+		t.Fatal(err)
 	}
 
-	err = ledger.ValidateChain(signer, node.PublicKey)
-	if err != nil {
-		t.Fatal("PQC chain validation failed")
+	ledger := NewLedger(genesis, vs)
+
+	// --- Create next block with transaction ---
+	tx := transaction.NewTransaction(
+		node,
+		"block_payload",
+		"block_data",
+	)
+
+	if err := tx.SignWithIdentity(node); err != nil {
+		t.Fatal(err)
+	}
+
+	newBlock := block.NewBlock(
+		1,
+		genesis.Hash,
+		[]*transaction.Transaction{tx},
+	)
+
+	if err := newBlock.Finalize(node); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := ledger.AddBlock(newBlock, signer, node.PublicKey); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := ledger.ValidateChain(signer); err != nil {
+		t.Fatal("Chain validation failed:", err)
 	}
 }
