fd31322689ae5aa0124203d7dbb5e256944ac5fe
sureshKrishna05 <sureshkrishnar05@gmail.com>
2026-03-02 09:04:54 +0530

    Benchmark

--- core/crypto/dilithium_benchmark_test.go
+++ core/crypto/dilithium_benchmark_test.go
@@ -2,6 +2,10 @@ package crypto
 
 import "testing"
 
+//////////////////////////////
+// DILITHIUM BENCHMARKS
+//////////////////////////////
+
 func BenchmarkDilithiumKeyGen(b *testing.B) {
 	signer, err := NewDilithiumSigner()
 	if err != nil {
@@ -49,6 +53,76 @@ func BenchmarkDilithiumVerify(b *testing.B) {
 		b.Fatal(err)
 	}
 	defer signer.Close()
+
+	pub, priv, err := signer.GenerateKeyPair()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	msg := []byte("Benchmark Message")
+
+	sig, err := signer.Sign(priv, msg)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		if !signer.Verify(pub, msg, sig) {
+			b.Fatal("verify failed")
+		}
+	}
+}
+
+//////////////////////////////
+// ECDSA BENCHMARKS
+//////////////////////////////
+
+func BenchmarkECDSAKeyGen(b *testing.B) {
+	signer, err := NewECDSASigner()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _, err := signer.GenerateKeyPair()
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkECDSASign(b *testing.B) {
+	signer, err := NewECDSASigner()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	_, priv, err := signer.GenerateKeyPair()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	msg := []byte("Benchmark Message")
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := signer.Sign(priv, msg)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkECDSAVerify(b *testing.B) {
+	signer, err := NewECDSASigner()
+	if err != nil {
+		b.Fatal(err)
+	}
 
 	pub, priv, err := signer.GenerateKeyPair()
 	if err != nil {
