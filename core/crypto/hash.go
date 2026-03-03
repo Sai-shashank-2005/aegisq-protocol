7d9a96e3c02a9d6415a337208851900ebd79a934
sureshKrishna05 <sureshkrishnar05@gmail.com>
2026-03-03 22:04:54 +0530

    Optimization #1

--- core/crypto/hash.go
+++ core/crypto/hash.go
@@ -1,12 +1,11 @@
 package crypto
 
 import (
-	"golang.org/x/crypto/sha3"
+	"crypto/sha256"
 )
 
 // Hash computes SHA3-256 hash of input data.
 func Hash(data []byte) []byte {
-	h := sha3.New256()
-	h.Write(data)
-	return h.Sum(nil)
+	sum := sha256.Sum256(data)
+	return sum[:]
 }
